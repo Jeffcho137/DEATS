281839c2f72800a53bee5df1214b0b21acb9f25e
Kathy Cui <kathycui@Kathys-MacBook-Air.local>
2022-02-09 14:24:05 -0500

    home page created

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import styles from './style';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.home_text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+
 }
