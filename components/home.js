d496e2053dcc2467f84cec9f8c7f72c56f7f241c
Brian Yu <brianyu@Brians-MacBook-Air.local>
2022-02-09 15:54:40 -0500

    order selection page added

--- components/home.js
+++ components/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button } from 'react-native';
 import styles from '../style';
+import { Order_selection } from './order_selection';
 
 export class Home extends Component {
     render() {
@@ -9,7 +10,7 @@ export class Home extends Component {
             <View style={styles.container}>
                 <Text style={styles.home_text}>this is the home page</Text>
                 <Button title="Profile" onPress={() => this.props.navigation.navigate('Profile')}></Button>
-                <Button title="Order Delivery"></Button>
+                <Button title="Order Delivery" onPress={() => this.props.navigation.navigate('Order_selection')}></Button>
                 <Button title="Make Delivery"></Button>
                 <StatusBar style="auto" />
             </View>
