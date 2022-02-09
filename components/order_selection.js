7b326309108a139e1312f84409906161b8929055
Kathy Cui <kathycui@Kathys-MacBook-Air.local>
2022-02-09 17:09:43 -0500

    order selection skeleton

--- components/order_selection.js
+++ components/order_selection.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
 import styles from '../style';
 
 export class Order_selection extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.home_text}>this is the order selection page</Text>
+                <View style={styles.order_sel}>
+                    <Text style={styles.order_sel_text}>I want food from:</Text>
+                    <View style={styles.order_selection_place_options}>
+                        <Button title='HOP'></Button>
+                        <Button title='Collis'></Button>
+                    </View>
+                </View>
+                <View style={styles.order_sel_input}>
+                    <Text style={styles.order_sel_text}>Deliver to:</Text>
+                    <View style={styles.order_sel_input_box}>
+                        <TextInput style={styles.single_input} placeholder='building name or street address'></TextInput>
+                        <TextInput style={styles.single_input} placeholder='room number'></TextInput>
+                        <TextInput style={styles.single_input} placeholder='phone number'></TextInput>
+                    </View>
+                </View>
+                <View>
+                    <Text>I want my food between</Text>
+                    <View>
+                        <TextInput placeholder='time'></TextInput>
+                        <Text>and</Text>
+                        <TextInput placeholder='time'></TextInput>
+                    </View>
+                </View>
                 <Button title="Confirm" onPress={() => this.props.navigation.navigate('Profile')}></Button>
             
                 <StatusBar style="auto" />
--- style.js
+++ style.js
@@ -10,6 +10,44 @@ const styles = StyleSheet.create({
 
     home_text: {
         color: "#323",
+    },
+
+    order_sel: {
+      width: 300,
+      alignItems: 'center',
+      // backgroundColor: "coral",
+      marginBottom: 10,
+      // flexDirection: 'row',
+
+    },
+
+    order_sel_text: {
+      alignSelf: 'flex-start',
+    },
+
+    order_selection_place_options: {
+      flexDirection: 'row',
+      // backgroundColor: "white",
+      alignItems: 'center',
+    }, 
+
+    order_sel_input: {
+      width: 300,
+      alignItems: 'center',
+      // backgroundColor: "coral",
+      marginBottom: 10,
+      flexDirection: 'row',
+
+    }, 
+
+    order_sel_input_box: {
+      // backgroundColor: "white",
+      marginLeft: 30,
+    }, 
+
+    single_input: {
+      borderRadius: 3,
+      borderWidth: 1,
     }
 })
 
