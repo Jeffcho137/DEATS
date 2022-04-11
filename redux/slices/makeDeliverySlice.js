3a8d5826f1f73071d34edd3818c290a87ae2a7fe
Edmund Aduse Poku <edmundapoku@github.com>
2022-04-11 03:19:57 -0400

    Set up makeDelivrySlice for react-redux to manage making deliveries

---
+++ redux/slices/makeDeliverySlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    startingPoint: null,
+    destination: null,
+    unmatchedCustomers: null,
+    selectedCustomer: null
+}
+
+export const makeDeliverySlice = createSlice({
+    name: "makeDeliverySlice",
+    initialState: {initialState}, 
+    reducers: {
+        setStartingPoint: (state, action) => {
+            state.startingPoint = action.payload;
+        },
+
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+
+        setUnmatchedCustomers: (state, action) => {
+            state.unmatchedCustomers = action.payload;
+        },
+
+        setSelectedCustomer: (state, action) => {
+            state.selectedCustomer = action.payload;
+        }
+    }
+})
+
+export const selectSetStartingPoint = (state) => state.makeDeliverySlice.startingPoint
+export const selectSetDestination = (state) => state.makeDeliverySlice.destination
+export const selectSetUnmatchedCustomers = (state) => state.makeDeliverySlice.unmatchedCustomers
+export const selectSetSelectedCustomer = (state) => state.makeDeliverySlice.selectedCustomer
+export const { setStartingPoint, setDestination, setUnmatchedCustomers, setSelectedCustomer } = makeDeliverySlice.actions
+export default userSlice.reducer
