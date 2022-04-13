1b3c541804e977c2ce5678bb3fe384accfb03989
Edmund Aduse Poku <edmundapoku@github.com>
2022-04-12 22:11:49 -0400

    Re-write completed.js as a functional component

--- components/completed.js
+++ components/completed.js
@@ -3,22 +3,36 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button, TextInput } from 'react-native';
 import styles from '../style';
 
-export class Completed extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.order_sel}>
-                    <Text style={styles.searching_text}>Thanks for using DEATS!</Text>
+export function Completed (props) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.order_sel}>
+                <Text style={styles.searching_text}>Thanks for using DEATS!</Text>
+            </View>
+            <Button title="Back to home" onPress={() => props.navigation.navigate('Home')}></Button>
+            <StatusBar style="auto" />
+        </View>
+    )
+    
+}
+
+
+// export class CompletedC extends Component {
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//                 <View style={styles.order_sel}>
+//                     <Text style={styles.searching_text}>Thanks for using DEATS!</Text>
                     
 
-                </View>
+//                 </View>
                 
                 
-                <Button title="Back to home" onPress={() => this.props.navigation.navigate('Home')}></Button>
+//                 <Button title="Back to home" onPress={() => this.props.navigation.navigate('Home')}></Button>
 
             
-                <StatusBar style="auto" />
-            </View>
-        )
-    }
-}
+//                 <StatusBar style="auto" />
+//             </View>
+//         )
+//     }
+// }
