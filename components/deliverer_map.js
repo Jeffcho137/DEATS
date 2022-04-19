c309e80f0a452ef1016d2b7fc02dd0219750f54a
Edmund Aduse Poku <edmundapoku@github.com>
2022-04-19 03:47:03 -0400

    Resolve location address bug for deliverer_map

--- components/deliverer_map.js
+++ components/deliverer_map.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useRef, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Button, TextInput, Dimensions } from "react-native";
 import styles from "../style";
@@ -46,25 +46,34 @@ const get_fin_location = (lat, long) => {
 
 // export class Map_test extends Component {
 const Del_map = (props) => {
-  //render() {
-  const [startPin, setstartPin] = React.useState({
-    latitude: 43.704483237221815,
-    longitude: -72.28869350196095,
+  let markerRef = useRef(null);
+  const [calloutMounted, setCalloutMounted] = useState(false);
+  const [startPinDragged, setFinPinDragged] = useState(false);
+  const [startPinSelected, setStartPinSelected] = useState(false);
+  const [finPinDragged, setStartPinDragged] = useState(false);
+  const [finPinSelected, setFinPinSelected] = useState(false);
+
+  useEffect(() => {
+    console.log("callMounted:", calloutMounted);
+    markerRef.current && calloutMounted && markerRef.current.redrawCallout();
   });
 
-  const [finPin, setfinPin] = React.useState({
-    latitude: 43.704483237221815,
-    longitude: -72.28869350196095,
-  });
+  useEffect(() => {
+    get_start_location(startRegion.latitude, startRegion.longitude);
+  }, [startPinDragged]);
 
-  const [startRegion, setstartRegion] = React.useState({
+  useEffect(() => {
+    get_fin_location(finRegion.latitude, finRegion.longitude);
+  }, [finPinDragged]);
+
+  const [startRegion, setStartRegion] = React.useState({
     latitude: 43.704483237221815,
     longitude: -72.28869350196095,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   });
 
-  const [finRegion, setfinRegion] = React.useState({
+  const [finRegion, setFinRegion] = React.useState({
     latitude: 43.704483237221815,
     longitude: -72.28869350196095,
     latitudeDelta: 0.01,
@@ -80,9 +89,7 @@ const Del_map = (props) => {
           rankby: "distance",
         }}
         onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          //console.log(data, details);
-          setstartRegion({
+          setStartRegion({
             latitude: details.geometry.location.lat,
             longitude: details.geometry.location.lng,
             latitudeDelta: 0.01,
@@ -90,14 +97,6 @@ const Del_map = (props) => {
           });
           start_location = details.name +"\n"+ details.formatted_address
           console.log("start_loc", start_location)
-        //   get_start_location(startPin.latitude, startPin.longitude);
-        //   console.log("search", start_location);
-
-        //   setstartPin({
-        //     latitude: details.geometry.location.lat,
-        //     longitude: details.geometry.location.lng,
-        //   })
-
         }}
 
         query={{
@@ -106,12 +105,10 @@ const Del_map = (props) => {
           components: "country:us",
           radius: 300,
           location: `${startRegion.latitude}, ${startRegion.longitude}`,
-          // types: establishment,
         }}
         styles={{
           container: {
             flex: 0,
-            // position: "absolute",
             width: "100%",
             zIndex: 1,
           },
@@ -125,33 +122,19 @@ const Del_map = (props) => {
           rankby: "distance",
         }}
         onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
           console.log(data);
           console.log('-----')
           console.log(details)
-          setfinRegion({
+          setFinRegion({
             latitude: details.geometry.location.lat,
             longitude: details.geometry.location.lng,
             latitudeDelta: 0.01,
             longitudeDelta: 0.01,
-            
           });
           fin_location = details.name +"\n"+ details.formatted_address
           console.log("finloc", fin_location)
 
-        //   setfinPin({
-        //     latitude: data.nativeEvent.coordinate.latitude,
-        //     longitude: data.nativeEvent.coordinate.longitude,
-        //     // latitude: details.geometry.location.lat,
-        //     // longitude: details.geometry.location.lng,
-        //   })
-          //get_fin_location(finPin.latitude, finPin.longitude);
-        //   get_fin;_location(finRegion.latitude, finRegion.longitude);
-
-
         console.log("search", fin_location)
-
-
         }}
 
         query={{
@@ -160,12 +143,10 @@ const Del_map = (props) => {
           components: "country:us",
           radius: 300,
           location: `${finRegion.latitude}, ${finRegion.longitude}`,
-          // types: establishment,
         }}
         styles={{
           container: {
             flex: 0,
-            // position: "absolute",
             width: "100%",
             zIndex: 1,
           },
@@ -181,16 +162,8 @@ const Del_map = (props) => {
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         }}
-        // region={startRegion}
       >
-        {/* <Marker
-          coordinate={{
-            latitude: region.latitude,
-            longitude: region.longitude,
-          }}
-        /> */}
         <Marker
-          coordinate={startPin}
           coordinate={{
             latitude: startRegion.latitude,
             longitude: startRegion.longitude,
@@ -202,12 +175,10 @@ const Del_map = (props) => {
           onPress={(e) => console.log(e.nativeEvent.coordinate)}
           onDragEnd={(e) => {
             console.log("Drag end", e.nativeEvent.coordinate);
-            setstartPin({
+            setStartRegion({
               latitude: e.nativeEvent.coordinate.latitude,
               longitude: e.nativeEvent.coordinate.longitude,
             });
-
-            get_start_location(startPin.latitude, startPin.longitude);
           }}
         >
           <Callout>
@@ -218,8 +189,6 @@ const Del_map = (props) => {
           </Callout>
         </Marker>
         <Marker
-            style={{backgroundColor: 'blue'}}
-          coordinate={finPin}
           coordinate={{
             latitude: finRegion.latitude,
             longitude: finRegion.longitude,
@@ -231,12 +200,10 @@ const Del_map = (props) => {
           onPress={(e) => console.log(e.nativeEvent.coordinate)}
           onDragEnd={(e) => {
             console.log("Drag end", e.nativeEvent.coordinate);
-            setfinPin({
+            setFinRegion({
               latitude: e.nativeEvent.coordinate.latitude,
               longitude: e.nativeEvent.coordinate.longitude,
             });
-
-            get_fin_location(finPin.latitude, finPin.longitude);
           }}
         >
           <Callout>
@@ -246,19 +213,17 @@ const Del_map = (props) => {
             </Text>
           </Callout>
         </Marker>
-        {/* <Circle center={startPin} radius={100} /> */}
       </MapView>
 
       <Button
-        // styles={{ flex: 1 }}
         title="Confirm"
         onPress={() => props.navigation.navigate("DeliverySelection",{
           chosen: true,
-          start_lat: startPin.latitude,
-          start_long: startPin.longitude,
+          start_lat: startRegion.latitude,
+          start_long: startRegion.longitude,
           address1: start_location,
-          fin_lat: finPin.latitude,
-          fin_long: finPin.longitude,
+          fin_lat: finRegion.latitude,
+          fin_long: finRegion.longitude,
           address2: fin_location,
         })}
       ></Button>
