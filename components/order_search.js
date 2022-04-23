05b8e64b400eccd95bd8e5b68c69e0e331232a1f
Edmund Aduse Poku <edmundapoku@github.com>
2022-04-22 20:40:00 -0400

    Modify order_search.js to use react-redux

--- components/order_search.js
+++ components/order_search.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, TextInput } from 'react-native';
-import { Alert, Modal, StyleSheet, Pressable, } from "react-native";
+import { Text, View, Button } from 'react-native';
+import { Alert, Modal, Pressable, } from "react-native";
 import styles from '../style';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDelivererId, selectDelivererInfo, selectOrderId, selectPickupLocation, setDelivererId, setDelivererInfo } from '../redux/slices/orderDeliverySlice';
 
-export function Order_search (props) {
-    const order_id = props.navigation.state.params.order_id
-    const foodPlaceName = props.navigation.state.params.food_place_name
+export function Order_search ({ navigation }) {
+    const dispatch = useDispatch()
+    const orderId = useSelector(selectOrderId)
+    const delivererId = useSelector(selectDelivererId)
+    const delivererInfo = useSelector(selectDelivererInfo)
 
     const [modalVisible, setModalVisible] = useState(false)
-    const [delInfo, setDelInfo] = useState(null)
-    const [delId, setDelId] = useState("")
     
     const findDrivers = () => {
+      console.log("orderId", orderId)
       fetch('https://deats-backend-test.herokuapp.com/my_deliverer/',
         {
             method: 'POST',
@@ -21,7 +24,7 @@ export function Order_search (props) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                order_id: order_id,
+                order_id: orderId,
             })
         })
         .then(response => response.json())
@@ -29,8 +32,8 @@ export function Order_search (props) {
             console.log(data)
             console.log(data.deliverer_info)
             if (data.succeeded == true) {
-              setDelInfo(data.deliverer_info)
-              setDelId(data.deliverer_id)
+              dispatch(setDelivererInfo(data.deliverer_info))
+              dispatch(setDelivererId(data.deliverer_id))
               setModalVisible(true)
             } else {
                 console.log(data.msg);
@@ -53,23 +56,18 @@ export function Order_search (props) {
             <View style={styles.centeredView}>
                 <View style={styles.modalView}>
                   <View style={styles.modal_text}>
-                    <Text style={styles.modalText}>{delId ? "Deliverer found!" : "Sorry, no deliverer is available yet"}</Text>
-                    <Text style={styles.deliverer_is}> {delId ? "Your deliverer is " + delInfo.name : "Check again later"}</Text>
-                    <Text style={styles.deliverer_is}> {delId ? "Delivery fee: $3.00" : ""} </Text>
+                    <Text style={styles.modalText}>{delivererId ? "Deliverer found!" : "Sorry, no deliverer is available yet"}</Text>
+                    <Text style={styles.deliverer_is}> {delivererId ? "Your deliverer is " + delivererInfo.name : "Check again later"}</Text>
+                    <Text style={styles.deliverer_is}> {delivererId ? "Delivery fee: $3.00" : ""} </Text>
                   </View>
                   <Pressable
                       style={[styles.button, styles.buttonClose]}
-                      onPress={() => {setModalVisible(!modalVisible); {delId ? props.navigation.navigate('OrderMatch', 
-                      {
-                        delId: delId,
-                        delInfo: delInfo,
-                        food_place_name: foodPlaceName
-                    }) : ""}}}
+                      onPress={() => {setModalVisible(!modalVisible); {delivererId ? navigation.navigate('OrderMatch') : ""}}}
                   >
-                    <Text style={styles.textStyle}> {delId ? "Accept Delivery" : "Go back"} </Text>
+                    <Text style={styles.textStyle}> {delivererId ? "Accept Delivery" : "Go back"} </Text>
                   </Pressable>
                   <Pressable
-                      onPress={() => {setModalVisible(!modalVisible); props.navigation.navigate('Home')}}
+                      onPress={() => {setModalVisible(!modalVisible); navigation.navigate('Home')}}
                   >
                     <Text style={{textAlign: 'center',textDecorationLine: 'underline',marginTop: 5}}>cancel order request (this cannot be undone)</Text>
                   </Pressable>
@@ -81,136 +79,8 @@ export function Order_search (props) {
                 <Text style={styles.searching_text}>SEARCHING...</Text>
             </View>
             <Button color="#006400" title="Refresh" onPress={findDrivers}></Button>
-            <Button color="#006400" title="Cancel" onPress={() => props.navigation.navigate('Home')}></Button>
+            <Button color="#006400" title="Cancel" onPress={() => navigation.navigate('Home')}></Button>
             <StatusBar style="auto" />
         </View> 
     )
 }
-
-
-// export class Order_searchC extends Component {
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//         id: this.props.navigation.state.params.id,
-//         name: this.props.navigation.state.params.name,
-//         number: this.props.navigation.state.params.number,
-//         email: this.props.navigation.state.params.email,
-//         password: this.props.navigation.state.params.password,
-//         user_type: this.props.navigation.state.params.user_type,
-//         order_id: this.props.navigation.state.params.order_id,
-//         food_place_name: this.props.navigation.state.params.food_place_name,
-//         modalVisible: false,
-//         del_info: null,
-//         del_id: null
-//     }
-//   }
-
-//   componentDidMount() {
-//     console.log("componentDidMount fired");
-//     console.log("STATE", this.state);
-//   }
-
-//   componentDidUpdate() {
-//       console.log("componentDidUpdate fired");
-//       console.log("STATE", this.state);
-//   }
-    
-//     setModalVisible = (visible) => {
-//       this.setState({ modalVisible: visible });
-//     }
-
-//     findDrivers = () => {
-//       fetch('https://deats-backend-test.herokuapp.com/my_deliverer/',
-//         {
-//             method: 'POST',
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({
-//                 order_id: this.state.order_id,
-//             })
-//         })
-//         .then(response => response.json())
-//         .then((data) => {
-//             console.log(data)
-//             console.log(data.deliverer_info)
-//             if (data.succeeded == true) {
-//               this.setState({
-//                 del_info: data.deliverer_info,
-//                 del_id: data.deliverer_id
-//               })
-//               this.setModalVisible(true)
-//               // this.props.navigation.navigate('OrderMatch');
-//             } else {
-//                 console.log(data.msg);
-//             }
-//         })
-//         .catch(err => console.error(err));
-//     }
-
-//     render() {
-//           const modalVisible = this.state.modalVisible;
-//         return (
-//             <View style={styles.container}>
-//                 <Modal
-//                     animationType="slide"
-//                     transparent={true}
-//                     visible={modalVisible}
-//                     onRequestClose={() => {
-//                     Alert.alert("Modal has been closed.");
-//                     this.setModalVisible(!modalVisible);
-//                 }}
-//                 >
-//                 <View style={styles.centeredView}>
-//                     <View style={styles.modalView}>
-//                       <View style={styles.modal_text}>
-//                         <Text style={styles.modalText}>{this.state.del_id ? "Deliverer found!" : "Sorry, no deliverer is available yet"}</Text>
-//                         <Text style={styles.deliverer_is}> {this.state.del_id ? "Your deliverer is " + this.state.del_info.name : "Check again later"} {}</Text>
-//                         <Text style={styles.deliverer_is}> {this.state.del_id ? "Delivery fee: $3.00" : ""} </Text>
-//                       </View>
-//                       {/* <Button title='Accept' onPress={this.props.navigation.navigate('OrderMatch')}></Button> */}
-//                       <Pressable
-//                           style={[styles.button, styles.buttonClose]}
-//                           // onPress={() => this.setModalVisible(!modalVisible)}
-//                           onPress={() => {this.setModalVisible(!modalVisible); {this.state.del_id ? this.props.navigation.navigate('OrderMatch', 
-//                           {
-//                             del_id: this.state.del_id,
-//                             del_info: this.state.del_info,
-//                             food_place_name: this.state.food_place_name
-//                         }) : ""}}}
-//                       >
-//                         <Text style={styles.textStyle}> {this.state.del_id ? "Accept Delivery" : "Go back"} </Text>
-//                       </Pressable>
-//                       <Pressable
-//                           // onPress={() => this.setModalVisible(!modalVisible)}
-//                           onPress={() => {this.setModalVisible(!modalVisible); this.props.navigation.navigate('Home')}}
-//                       >
-//                         <Text style={{textAlign: 'center',textDecorationLine: 'underline',marginTop: 5}}>cancel order request (this cannot be undone)</Text>
-//                       </Pressable>
-//                       {/* <Button title='cancel order (this cannot be undone)' onPress={this.props.navigation.navigate('Home')}></Button> */}
-//                     </View>
-//                   </View>
-//                 </Modal>
-//                 {/* <Pressable
-//                   style={[styles.button, styles.buttonOpen]}
-//                   onPress={() => this.setModalVisible(true)}
-//                 >
-//                   <Text style={styles.textStyle}>Who's my Driver?</Text>
-//                 </Pressable> */}
-//                 <View style={styles.order_sel}>
-//                     <Text style={styles.searching_text}>SEARCHING...</Text>
-//                 </View>
-//                 <Button color="#006400" title="Refresh" onPress={this.findDrivers}></Button>
-//                 <Button color="#006400" title="Cancel" onPress={() => this.props.navigation.navigate('Home')}></Button>
-
-            
-//                 <StatusBar style="auto" />
-
-                
-//             </View>
-            
-//         )
-//     }
-// }
--- redux/slices/orderDeliverySlice.js
+++ redux/slices/orderDeliverySlice.js
@@ -3,7 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     pickupLocation: null,
     dropLocation: null,
-    orderId: null
+    orderId: null,
+    delivererId: null,
+    delivererInfo: null
 }
 
 export const orderDeliverySlice = createSlice({
@@ -20,6 +22,14 @@ export const orderDeliverySlice = createSlice({
 
         setOrderId: (state, action) => {
             state.orderId = action.payload;
+        },
+
+        setDelivererId: (state, action) => {
+            state.delivererId = action.payload;
+        },
+
+        setDelivererInfo: (state, action) => {
+            state.delivererInfo = action.payload;
         }
     }
 })
@@ -27,5 +37,7 @@ export const orderDeliverySlice = createSlice({
 export const selectPickupLocation = (state) => state.orderDeliverySlice.pickupLocation
 export const selectDropLocation = (state) => state.orderDeliverySlice.dropLocation
 export const selectOrderId = (state) => state.orderDeliverySlice.orderId
-export const { setPickupLocation, setDropLocation, setOrderId } = orderDeliverySlice.actions
+export const selectDelivererId = (state) => state.orderDeliverySlice.delivererId
+export const selectDelivererInfo = (state) => state.orderDeliverySlice.delivererInfo
+export const { setPickupLocation, setDropLocation, setOrderId, setDelivererId, setDelivererInfo } = orderDeliverySlice.actions
 export default orderDeliverySlice.reducer
