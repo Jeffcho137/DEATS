8cd1596f444c7d2b636fc1ad52c51e8ea33af3e9
Brian Yu <brianyu@Brians-MacBook-Air.local>
2022-04-25 16:45:49 -0400

    deliver_match_status

--- components/deliver_match.js
+++ components/deliver_match.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button, TextInput } from 'react-native';
 import styles from '../style';
+import { useSelector } from 'react-redux';
+import { selectStartingPoint, selectDestination, setDestination } from '../redux/slices/makeDeliverySlice';
+import { selectDelivererInfo, selectPickupLocation, setPickupLocation, selectDropLocation } from '../redux/slices/orderDeliverySlice';
 
 export function Deliver_match (props) {
+    const pickUpLocation = useSelector(selectPickupLocation)
+    const dropLocation = useSelector(selectDropLocation)
+
     return (
         <View style={styles.container}>
             <View style={styles.order_sel}>
-                <Text style={styles.searching_text}>Deliver to LSC from HOP</Text>
+                <Text style={styles.searching_text}>Deliver to {dropLocation.address} from {pickUpLocation.address}</Text>            
             </View>
             <Button title="Confirm" onPress={() => props.navigation.navigate('DeliverStatus')}></Button>
             <StatusBar style="auto" />
