80a7d050255ebb6d45141805358502f304f95dd8
Edmund Aduse Poku <edmundapoku@github.com>
2022-05-20 04:48:24 -0400

    Initial notifications-system set up

--- components/home.js
+++ components/home.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, Button } from 'react-native';
 import styles from '../style';
 import  Logo  from './image_small.js';
 import TabBarBottom from './tab_bar_bottom';
+import DEATSNotifications from './notifications';
 
 export function Home ({ navigation }) {
+    DEATSNotifications()
     return (
         <View style={styles.container}>
             <View style={styles.home_profile_button}>
---
+++ components/notifications.js
@@ -0,0 +1,82 @@
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { useState, useEffect, useRef } from 'react';
+import { Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export default function DEATSNotifications() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+      console.log("DEATS notifications", notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log("DEATS notifications response", response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+}
+
+async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "New message 🍲",
+        body: "Your food is hear",
+        data: { DEATS: "notifications" },
+      },
+      trigger: { seconds: 1 },
+    });
+  }
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Push Notifications require a physical device!');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
