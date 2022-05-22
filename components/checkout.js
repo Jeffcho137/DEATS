f5ef72b41e2a76cb4260f79071c3c1982d628ee4
Edmund Aduse Poku <edmundapoku@github.com>
2022-05-22 17:38:50 -0400

    Functional checkout.js

--- components/checkout.js
+++ components/checkout.js
@@ -1,72 +1,81 @@
 import { useStripe } from "@stripe/stripe-react-native";
 import { useEffect, useState } from "react";
-import { Alert, Button } from "react-native";
+import { Alert } from "react-native";
+import { Button } from "react-native-elements";
+
 import { Screen } from "react-native-screens";
 import { DEATS_SERVER_URL, ROUTE_CHECKOUT } from "../utils/Constants";
 
 export default function Checkout() {
     const { initPaymentSheet, presentPaymentSheet } = useStripe();
     const [loading, setLoading] = useState(false);
-
-    fetchPaymentParams = () => {
-        fetch(`${DEATS_SERVER_URL}${ROUTE_CHECKOUT}`,
-        {
+  
+    const fetchPaymentSheetParams = async () => {
+        const response = await fetch(`${DEATS_SERVER_URL}${ROUTE_CHECKOUT}`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+            },
+        });
+        const { paymentIntent, ephemeralKey, customer} = await response.json();
+        return {
+            paymentIntent,
+            ephemeralKey,
+            customer,
+        };
+    };
+  
+    const initializePaymentSheet = async () => {
+        const {
+            paymentIntent,
+            ephemeralKey,
+            customer,
+        } = await fetchPaymentSheetParams().catch(() => {
+            Alert.alert(
+            "Error",
+            "Could not connect to server. Please try again later.",
+            [{ text: "OK" }],
+            { cancelable: false }
+            );
+            
+            setLoading(false);
+        });
+        const { error } = await initPaymentSheet({
+            customerId: customer,
+            customerEphemeralKeySecret: ephemeralKey,
+            paymentIntentClientSecret: paymentIntent,
+            allowsDelayedPaymentMethods: false,  // don't handle payment methods that complete payment after a delay, like SEPA Debit and Sofort
+            onPaymentSuccess: (customerId) => {
+                console.log("Payment Successful");
+                console.log(customerId);
             }
-        })
-        .then(response => response.json())
-        .then((data) => {
-            console.log("server response:", data);
-            return data;
-        })
-        .catch(error => console.log("Initiate payment error: ", error));
-    }
-  
-    const initializePayment = async () => {
-      const {
-        paymentIntent,
-        ephemeralKey,
-        customer,
-      } = await fetchPaymentParams();
-  
-      const { error } = await initPaymentSheet({
-        customerId: customer,
-        customerEphemeralKeySecret: ephemeralKey,
-        paymentIntentClientSecret: paymentIntent,
-        allowsDelayedPaymentMethods: true, // handle payment methods that complete payment after a delay, like SEPA Debit and Sofort
-      });
-
-      if (!error) {
+        });
+        if (!error) {
         setLoading(true);
       }
     };
-
-    const openPayment = async () => {
+  
+    const openPaymentSheet = async () => {
         const { error } = await presentPaymentSheet();
-    
         if (error) {
-            console.log("Open payment error:", error);
-            Alert.alert(`Error code: ${error.code}`, error.message);
+          Alert.alert(`Error code: ${error.code}`, error.message);
         } else {
-            Alert.alert('Success', 'Your order is confirmed!');
+          Alert.alert('Success', 'Your order is confirmed!');
         }
       };
-
   
     useEffect(() => {
-      initializePayment();
+      initializePaymentSheet();
     }, []);
   
     return (
       <Screen>
         <Button
           variant="primary"
-          disabled={false}
+          disabled={!loading}
           title="Checkout"
-          onPress={openPayment}
+          onPress={openPaymentSheet}
         />
       </Screen>
     );
