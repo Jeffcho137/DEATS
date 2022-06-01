2cc4110ce9ebb656581a82585bce3adeba953e40
Kathy Cui <kathycui@Kathys-MacBook-Air.local>
2022-06-01 00:34:28 -0400

    styling changes

--- components/order_status.js
+++ components/order_status.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 import { selectOrderId, selectDelivererInfo, selectPickupLocation,selectOrderStatus } from '../redux/slices/orderDeliverySlice';
 import { selectId } from '../redux/slices/userSlice';
 import { useClientSocket } from './client_socket';
+import { COLOR_CROCODILE } from '../utils/Constants';
 
 export function Order_status ({ navigation }) {
     const orderId = useSelector(selectOrderId)
@@ -22,10 +23,33 @@ export function Order_status ({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.order_sel}>{status?<Text>{status}</Text>:<Text>Matched!</Text>}</View>
-      <Button title="GET" onPress={() => navigation.navigate("OrderCode")}></Button>
-      <Button title="Review Order" onPress={() => navigation.navigate("OrderReview")}></Button>
-      <Button title="I got my food!" onPress={() => navigation.navigate("Completed")}></Button>
+      <View style={{borderWidth: 1, borderRadius: 15, borderColor: COLOR_CROCODILE, width: '80%',
+      alignItems: 'center', 
+      justifyContent: 'center',
+      marginBottom: 20,
+      padding: 20,
+      backgroundColor: 'lightgrey'
+      }}>
+        <Text style={{fontSize: 20, color: COLOR_CROCODILE, fontWeight: 'bold', marginBottom: 10,}}>current status:</Text>
+        <View style={{fontSize: 20}}>{status?<Text style={{fontSize: 20, }}>{status}</Text>:<Text>Matched!</Text>}</View>
+      </View>
+      <Text style={{
+        width: '90%', 
+        textAlign: 'center', 
+        marginBottom: 50,
+      }}>your order status will update as your deliverer completes your order</Text>
+      
+      <View>
+        <View style={{}}>
+          <Button title="GET Code" onPress={() => navigation.navigate("OrderCode")}></Button>
+        </View>
+        <View style={{}}>
+          <Button title="Review My Order" onPress={() => navigation.navigate("OrderReview")}></Button>
+        </View>
+      </View>
+      <View style={{position: 'absolute', bottom: 50}}>
+        <Button title="I got my food!" onPress={() => navigation.navigate("Completed")}></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
